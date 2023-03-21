bb2ce354a333a495747a140c24e2debb11bd5f02
Kennylee Cintron <user@user-thinkpad-l15.fritz.box>
2023-03-21 10:43:56 +0100

    sign up func added

--- src/components/views/Signup.js
+++ src/components/views/Signup.js
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Signup() {
+  const [user, setUser] = useState({});
+  const [isUser, setIsUser] = useState(false);
+
+  console.log(user);
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    setIsUser(true);
+    return user;
+  };
   return (
     <>
-      <form>
-        <label>
-          Name & Lastname:
-          <input type="text" name="name" />
-        </label>
-        <br></br> <br></br>
-        <label>
-          Email:
-          <input type="text" name="email" />
-        </label>
-        <br></br>
-        <br></br>
-        <label>
-          Password:
-          <input type="password" name="email" />
-        </label>
-        <br></br>
-        <br></br>
-        <input type="submit" value="Submit" />
-      </form>
+      {isUser ? (
+        <h2>Welcome {user.userName}!</h2>
+      ) : (
+        <form onSubmit={handelSubmit}>
+          <label>
+            Name & Lastname:
+            <input
+              onChange={(e) => setUser({ ...user, userName: e.target.value })}
+              type="text"
+              name="name"
+              required
+            />
+          </label>
+          <br></br> <br></br>
+          <label>
+            Email:
+            <input
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              type="email"
+              name="email"
+              required
+            />
+          </label>
+          <br></br>
+          <br></br>
+          <label>
+            Password:
+            <input
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              type="password"
+              name="email"
+              required
+            />
+          </label>
+          <br></br>
+          <br></br>
+          <input type="submit" value="Submit" />
+        </form>
+      )}
     </>
   );
 }
