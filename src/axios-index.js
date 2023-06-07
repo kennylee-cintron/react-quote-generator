26b358fdd41253f3b63729fb8164628c002b32ec
Kennylee Cintron <user@user-thinkpad-l15.fritz.box>
2023-06-07 12:31:21 +0200

    added /post

--- src/axios-index.js
+++ src/axios-index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://server-message-board.onrender.com",
+  baseURL: "https://server-message-board.onrender.com/post",
   headers: {
     "Content-type": "application/json",
   },
